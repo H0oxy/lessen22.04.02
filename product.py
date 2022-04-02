37ca61e67a6b43405491b94f8aae977e2d7f49f3
H0oxy <70875505+H0oxy@users.noreply.github.com>
2022-04-02 11:56:45 +0500

    Add files via upload

--- product.py
+++ product.py
@@ -1,14 +1,15 @@
 products = [1, 45, 87, 2, 5, 23, 9, 4]
 seen_prods = [87, 1, 5]
-d = []
-def filter_products(products, seen_prods):
-    for item in products, seen_prods:
+
+def filter_products(products):
+    d = []
+    for item in products:
         if item not in d:
             d.append(item)
+        return d
 
 
 
 print(filter_products(
-        [products],
-        [seen_prods],
+    [products]
 ))
